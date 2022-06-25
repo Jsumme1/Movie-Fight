a4e5f0f91d59b848babc97ec41056fbff69145ac
Jsumme1 <88805050+Jsumme1@users.noreply.github.com>
2022-06-25 15:08:24 -0500

    added debouncer

--- index.js
+++ index.js
@@ -10,16 +10,35 @@ const fetchData = async (searchTerm) => {
 };
 
 const input = document.querySelector('input');
-// delaying search input - allows user to finish typing, limiting API requests
-let timeoutID;
-const onInput = event => {
-    // first time function is called - timeoutID is zero b/c it hasn't been invoked yet. New timeout is formed every time to avoid triggering the function for multiple api calls
-    if (timeoutID) {
-        clearTimeout(timeoutID);
-    }
-  timeoutID =  setTimeout(()=> {
-  fetchData(event.target.value);
-}, 1000);
+
+const debounce = (func, delay = 1000) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutID);
+        }
+        timeoutId = setTimeout(() => {
+            // .apply lets multiple arguments get passed individually to function
+            func.apply(null, args);
+        }, delay);
+     };
 };
 
-input.addEventListener('input', onInput);
+// original search delay - replaced by debouncer above
+// // delaying search input - allows user to finish typing, limiting API requests
+// let timeoutID;
+// const onInput = event => {
+//     // first time function is called - timeoutID is zero b/c it hasn't been invoked yet. New timeout is formed every time to avoid triggering the function for multiple api calls
+//     if (timeoutID) {
+//         clearTimeout(timeoutID);
+//     }
+//   timeoutID =  setTimeout(()=> {
+//   fetchData(event.target.value);
+// }, 1000);
+// };
+
+
+const onInput = event => {
+  fetchData(event.target.value);
+};
+input.addEventListener('input', debounce(onInput, 500));

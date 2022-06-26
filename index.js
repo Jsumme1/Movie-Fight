096323fba9ecd5a0f052027425f26202448d0caa
Jsumme1 <88805050+Jsumme1@users.noreply.github.com>
2022-06-26 13:55:12 -0500

    refactored to make autcomplete reusable

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 // use Axios for API call
-const fetchData = async searchTerm => {
+
+// autocomplete config object
+createAutoComplete({
+root:document.querySelector(".autocomplete"),
+renderOption (movie){
+  const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+  return `
+      <img src= "${imgSrc}"/>
+        ${movie.Title} (${movie.Year})
+    `;
+},
+onOptionSelect(movie) {
+    onMovieSelect(movie);
+},
+inputValue(movie) {
+    return movie.Title;
+},
+async fetchData(searchTerm ) {
     const response = await axios.get("http://www.omdbapi.com/", {
       params: {
         apikey: "a828aba8",
@@ -11,59 +28,7 @@ const fetchData = async searchTerm => {
         return [];
     }
     return response.data.Search;
-};
-
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-<label><b>Search for a Movie</b></label>
-<input class="input"/>
-<div class="dropdown">
-  <div class="dropdown-menu">
-     <div class="dropdown-content results">
-    </div>
-  </div>
-</div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector(".dropdown");
-const resultsWrapper = document.querySelector(".results");
-
-const onInput = async event => {
-const movies = await fetchData(event.target.value);
-
-// if nothing is in the input, drowdown disappears
-if (!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
 }
-
-resultsWrapper.innerHTML="";
-dropdown.classList.add('is-active');
- for (let movie of movies) {
-    const option = document.createElement('a');
-    const imgSrc = movie.Poster === "N/A" ? "" :movie.Poster
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
-        <img src= "${imgSrc}"/>
-        ${movie.Title}`;
-    option.addEventListener('click', () => {
-        dropdown.classList.remove('is-active');
-        input.value = movie.Title;
-        onMovieSelect(movie);
-    });
-
-    resultsWrapper.appendChild(option);
-    }
-};
-
-input.addEventListener('input', debounce(onInput, 500));
-
-// If user clicks anywhere but root element (drop down menu results) - drop down will disappear
-document.addEventListener("click", event => {
-    if (!root.contains(event.target)) {
-        dropdown.classList.remove('is-active')
-    }
 });
 
 
